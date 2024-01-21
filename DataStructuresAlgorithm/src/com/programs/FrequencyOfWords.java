af6ee2370fb485a832dc4b3b7c8f77f04ca7b444
AjeethaSampath <nikithasampath007@gmail.com>
2024-01-21 18:28:38 +0000

    Committing Frequencyofwords

--- DataStructuresAlgorithm/src/com/programs/FrequencyOfWords.java
+++ DataStructuresAlgorithm/src/com/programs/FrequencyOfWords.java
@@ -1,6 +1,12 @@
 package com.programs;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FrequencyOfWords {
 	 public static int[] numSmallerByFrequency(String[] queries, String[] words) {
@@ -33,12 +39,56 @@ public class FrequencyOfWords {
 		 return count;
 		 
 	 }
+	 public static String frequencySort(String s) {
+	        char[] ch = s.toCharArray();
+	        Map<String,Integer> unsortedMap = new HashMap<>();
+	        int count =0;
+	        String resultStr = "";
+	        Map<String,Integer> sortedMap = new LinkedHashMap<>();
+	        for (int i=0;i<ch.length;i++) {
+	        	if(!unsortedMap.containsKey(String.valueOf(ch[i]))) {
+	        		
+	        	count =0;	
+	        	count++;
+	        		unsortedMap.put(String.valueOf(ch[i]), count);
+	        
+	        }
+	        	else {
+	        		System.out.println(unsortedMap.get(String.valueOf(ch[i])));
+	        		count = unsortedMap.get(String.valueOf(ch[i]));
+	        		count++;
+	        		unsortedMap.replace(String.valueOf(ch[i]), count);
+	        	}
+	        }
+	        System.out.println(unsortedMap);
+	        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(unsortedMap.entrySet());
+	        entryList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+	        for(Map.Entry<String, Integer> entry:entryList) {
+	        	sortedMap.put(entry.getKey(), entry.getValue());
+	        }
+	        System.out.println(sortedMap);
+	        for (Map.Entry<String, Integer> entry : sortedMap.entrySet()) {
+				String key = entry.getKey();
+				Integer val = entry.getValue();
+				System.out.println(key+val);
+				while(val>0) {
+					resultStr+=key;
+					//resultStr.concat(key);
+					//System.out.println(resultStr);
+					val--;
+				}
+			}
+	        System.out.println(resultStr);
+	        return resultStr;
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String[] queries = {"bbb","cc"};
 		String[] words = {"a","aa","aaa","aaaa"};
-		int[] result = numSmallerByFrequency(queries, words);
-		System.out.println(Arrays.toString(result));
+		String str = "raaeaedere";//zzzzbbbeert
+		//int[] result = numSmallerByFrequency(queries, words);
+		//System.out.println(Arrays.toString(result));
+		System.out.println(frequencySort(str));
 
 	}
 
